04416a0a0cac2d80673647c4d28cb25456933573
Arju-Hasan <rajuahmeed698@gmail.com>
2025-10-18 03:04:07 +0600

    firebase init file deleted

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -8,23 +8,16 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_apiKey,
-//   authDomain: import.meta.env.VITE_authDomain,
-//   projectId: import.meta.env.VITE_projectId,
-//   storageBucket: import.meta.env.VITE_storageBucket,
-//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-//   appId: import.meta.env.VITE_appId,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAN9p6KZab0l4-pYUWnGQYcfx0e4NtiyDk",
-  authDomain: "fir-fighter-me.firebaseapp.com",
-  projectId: "fir-fighter-me",
-  storageBucket: "fir-fighter-me.firebasestorage.app",
-  messagingSenderId: "838236630009",
-  appId: "1:838236630009:web:5b52a8c888acecc810ab28"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
+
+
 
 
 
